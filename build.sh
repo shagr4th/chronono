207dd6242c13483f8e43437bc751b4e0b6c6dd37
shagrath <shagrath@pc4.home>
2018-10-30 09:36:38 +0100

    minimum requirement : el capitan

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #/bin/sh
 
-$GOPATH/bin/packr && go build -ldflags="-s -w"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export CGO_CFLAGS=-mmacosx-version-min=10.11
+    export CGO_CPPFLAGS=-mmacosx-version-min=10.11
+    export CGO_CXXFLAGS=-mmacosx-version-min=10.11
+    export CGO_LDFLAGS=-mmacosx-version-min=10.11
+fi
+
+$GOPATH/bin/packr && go build -a -ldflags="-s -w -v -extldflags=-mmacosx-version-min=10.11"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     #brew install upx
