ba100823cc566cdfc0ae38bb3bb36e6e01bfe037
shagrath <shagr4th@gmail.com>
2022-12-11 21:11:25 +0100

    Build fix

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #/bin/sh
 if [[ "$OSTYPE" == "darwin"* ]]; then
+
+    export CGO_CFLAGS=-mmacosx-version-min=10.11
+    export CGO_CPPFLAGS=-mmacosx-version-min=10.11
+    export CGO_CXXFLAGS=-mmacosx-version-min=10.11
+    export CGO_LDFLAGS=-mmacosx-version-min=10.11
+
     # https://dev.to/thewraven/universal-macos-binaries-with-go-1-16-3mm3
-    GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -a -ldflags="-s -w" -o chronono_amd64
-    GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -a -ldflags="-s -w" -o chronono_arm64
+    GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -a -ldflags="-s -w -extldflags=-mmacosx-version-min=10.11" -o chronono_amd64
+    GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -a -ldflags="-s -w -extldflags=-mmacosx-version-min=10.11" -o chronono_arm64
     lipo -create -output chronono chronono_amd64 chronono_arm64
     rm chronono_amd64 chronono_arm64
 
