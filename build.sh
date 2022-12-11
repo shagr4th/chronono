5e5c677d12986b94e6cc9b516155d158776337d3
shagrath <shagr4th@gmail.com>
2022-12-11 20:58:30 +0100

    universal binary

--- build.sh
+++ build.sh
@@ -1,24 +1,19 @@
 #/bin/sh
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    export CGO_CFLAGS=-mmacosx-version-min=10.11
-    export CGO_CPPFLAGS=-mmacosx-version-min=10.11
-    export CGO_CXXFLAGS=-mmacosx-version-min=10.11
-    export CGO_LDFLAGS=-mmacosx-version-min=10.11
-fi
+    # https://dev.to/thewraven/universal-macos-binaries-with-go-1-16-3mm3
+    GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -a -ldflags="-s -w" -o chronono_amd64
+    GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -a -ldflags="-s -w" -o chronono_arm64
+    lipo -create -output chronono chronono_amd64 chronono_arm64
+    rm chronono_amd64 chronono_arm64
 
-go build -a -ldflags="-s -w -v -extldflags=-mmacosx-version-min=10.11"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    #brew install upx
-    #disabled for the time being : https://github.com/upx/upx/issues/222
-    #upx chronono
     rm -rf chronono.app
     mkdir chronono.app
     mkdir chronono.app/Contents
     mkdir chronono.app/Contents/MacOS
     mkdir chronono.app/Contents/Resources
     cp osx/Infoplist chronono.app/Contents/Info.plist
-    cp chronono chronono.app/Contents/MacOS/
+    mv chronono chronono.app/Contents/MacOS/
     cp osx/icon.icns chronono.app/Contents/Resources/icon.icns
+else
+    go build -a -ldflags="-s -w"
 fi
